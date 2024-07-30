db1ce67f820cfdf3282ffe608c87b87b40662873
Cuong Le <tekielee@yahoo.com>
2024-07-30 09:11:08 -0700

    add classes for performance and monitor

---
+++ monitor/monitor.php
@@ -0,0 +1,57 @@
+<?php
+class MONITOR_WPM {
+    function __construct() {
+        
+    }
+
+    //scan wordpress plugins for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_plugins() {
+
+    }
+
+    //scan wordpress themes for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_themes() {
+    
+    }
+
+    //scan php for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_php() {
+    
+    }
+
+    //scan Apache for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_apache() {
+    
+    }
+
+    //scan nginx for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_nginx() {
+
+    }
+
+    //scan linux firewall for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_linux_firewall() {
+
+    }
+
+    //scan windows server firewall for vulnerabilities from public vulerability databases https://www.cvedetails.com/ and https://nvd.nist.gov/
+    public function scan_windows_firewall() {
+
+    }
+
+    //create the initial file hashes for the whole wordpress directory
+    public function create_file_hashes() {
+
+    }
+
+    //compare current files to the original file hashes and notify and allow users to replace them with the original
+    public function compare_files_to_hashes() {
+
+    }
+
+    //create cron job to provide continuous monitoring and performance check
+    public function create_cron_job() [
+        
+    ]
+}
+?>
---
+++ performance/performance.php
@@ -0,0 +1,28 @@
+<?php
+class PERFROMANCE_WPM {
+    function __construct() {
+        
+    }
+
+    //read apache to log to extract performance info
+    public function read_apache_logs() {
+
+    }
+
+    //use php reflection api to get class and object info
+    public function get_php_class_reflection() {
+
+    }
+
+    //get file('/proc/cpuinfo')
+    public function get_proc_cpuinfo() {
+
+    }
+
+    //get other proc files for performance checking
+    public function get_proc($file) {
+        
+    }
+}
+
+?>
